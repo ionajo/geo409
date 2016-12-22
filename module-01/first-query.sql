e3f45f0dbfbed0641b50d22a140fcf8e2a3cd138
tastyfreeze <boyd@townbranch.org>
2016-12-22 14:50:08 -0500

    Revert "remove helper comments"
    This reverts commit 2aee068c3067ecf6b61b3ef1ea3915101896acf6.

--- module-01/first-query.sql
+++ module-01/first-query.sql
@@ -1,3 +1,6 @@
+/* This is a comment line that helps us understand what our code does */
+
+/* We're going to construct an SQL statement to use later in QGIS. SQL (Structured Query Language) is how we'll interogate our spatial and attribute data. SQL is easy to learn, so let's try! */
 
 /* We want to select Kentucky from a polygon layer of US states. */
 
@@ -6,4 +9,4 @@ SELECT * FROM
 WHERE
     state_name = 'Kentucky'
     
-    
+/* Let's interpret what's happening. We have a table in our database called "state_polygon_layer" which contains columns for geometry, state name, and maybe other attributes. We're going to select all columns from this layer where the column name "state_name" equals the value "Kentucky" which will return a polygon for the Bluegrass state. */
